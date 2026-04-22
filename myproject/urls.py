d1ccbb2c427c9c5fc178945b4cbaf58260166dad
zaid <zaid@gmail.com>
2026-04-22 21:34:19 +0400

    اصلاح مسار الملفات  واضافة المكتبات

--- myproject/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include  # تأكد من استيراد include من django.urls
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('run.urls')),  # الطريقة الصحيحة لربط تطبيق run
-    path('accounts/', include('django.contrib.auth.urls')),
-]
